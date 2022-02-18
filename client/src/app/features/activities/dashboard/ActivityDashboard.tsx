44c413ebd7a35ef0a5ea227a6472aa42f8ef4a36
Awesomefusion <alexmould@hotmail.co.uk>
2022-02-18 14:26:51 +0000

    Added a filtering component and styled it.

--- client/src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { Grid, List } from 'semantic-ui-react';
 import LoadingComponent from '../../../layout/LoadingComponent';
 import { useStore } from '../../../stores/store';
+import ActivityFilters from './ActivityFilters';
 import ActivityList from './ActivityList';
 
 export default observer(function ActivityDashboard() {
@@ -24,7 +25,7 @@ export default observer(function ActivityDashboard() {
                 </List>
             </Grid.Column>
             <Grid.Column width='6'>
-                <h2>Activity Filters</h2>
+                <ActivityFilters/>
             </Grid.Column>
         </Grid>
     )
