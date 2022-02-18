eb45898687d46c5425c496242c7c28694922a523
Awesomefusion <alexmould@hotmail.co.uk>
2022-02-18 12:00:12 +0000

    Added css styling for each activity

--- client/src/app/features/activities/dashboard/ActivityListItem.tsx
+++ client/src/app/features/activities/dashboard/ActivityListItem.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Item, Label } from 'semantic-ui-react';
+import { Button, Icon, Item, ItemGroup, Label, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../models/activity';
 import { useStore } from '../../../stores/store';
 
@@ -19,30 +19,39 @@ export default function ActivityListItem({activity}: Props) {
         deleteActivity(id);
 }
     return (
-        <Item key={activity.id}>
-            <Item.Content>
-                <Item.Header as='a'>{activity.title}</Item.Header>
-                <Item.Meta>{activity.date}</Item.Meta>
-                <Item.Description>
-                    <div>
-                        {activity.description}
-                    </div>
-                    <div>
-                        {activity.city}, {activity.venue}
-                    </div>
-                </Item.Description>
-                <Item.Extra>
-                    <Button as={Link} to={`/activities/${activity.id}`} floated='right' content='View' color='blue'/>
-                    <Button
-                    name={activity.id}
-                    loading={loading && target === activity.id} 
-                    onClick={(e) => handleActivityDelete(e, activity.id)} 
-                    floated='right' 
-                    content='Delete' 
-                    color='red'/>
-                    <Label basic content={activity.category}/>
-                </Item.Extra>
-            </Item.Content>
-        </Item>
+        <Segment.Group>
+            <Segment>
+                <ItemGroup>
+                    <Item>
+                        <Item.Image size='tiny' circular src='/assets/user.png'/>
+                        <Item.Content>
+                            <Item.Header as={Link} to={`/activities/${activity.id}`}>
+                                {activity.title}
+                            </Item.Header>
+                        <Item.Description>Hosted by Bob</Item.Description>
+                        </Item.Content>
+                    </Item>
+                </ItemGroup>
+            </Segment>
+            <Segment>
+                <span>
+                    <Icon name='clock' /> {activity.date}
+                    <Icon name='marker'/> {activity.venue}
+                </span>
+            </Segment>
+            <Segment secondary>
+                Attendees go here
+            </Segment>
+            <Segment>
+                <span>{activity.description}</span>
+                <Button
+                    as={Link}
+                    to={`/activities/${activity.id}`}
+                    color='teal'
+                    floated='right'
+                    content='View'
+                />
+            </Segment>
+        </Segment.Group>
     )
 }
